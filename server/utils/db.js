2a76901595f039ef2b21b3200fde90975e4d48aa
Fernando Olguin <100617517+ferOlguiin@users.noreply.github.com>
2023-02-02 23:24:28 -0300

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,6 +3,7 @@ import {URI_MONGODB} from '../config.js';
 
 export async function dbConnect(){
     try {
+        mongoose.set('strictQuery',false);
         const db = await mongoose.connect(URI_MONGODB);
         console.log("Conectado a la base de datos: " + db.connection.name);
     } catch (error) {
