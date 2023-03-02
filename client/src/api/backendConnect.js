1d01f78c0fee4cf53fb69feb61699cf561b3931c
ferOlguiin <feergui997@hotmail.com>
2023-03-02 19:58:42 -0300

    clientChanges

--- client/src/api/backendConnect.js
+++ client/src/api/backendConnect.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
+const url = process.env.REACT_APP_API_BASE_URL;
 
-export const obtenerTareaRequest = async (id) => await axios.get("/tareas/" + id);
+export const obtenerTareaRequest = async (id) => await axios.get(`${url}/tareas/${id}`);
 
-export const obtenerUnaTareaRequest = async id => await axios.get("/tarea/" + id);
+export const obtenerUnaTareaRequest = async id => await axios.get(`${url}/tarea/${id}`);
 
-export const crearTareaRequest = async (nuevosCampos) => await axios.post("/creartarea", nuevosCampos);
+export const crearTareaRequest = async (nuevosCampos) => await axios.post(`${url}/creartarea`, nuevosCampos);
 
-export const editarTareaRequest = async (id, nuevosCampos) => await axios.put("/editar/" + id, nuevosCampos);
+export const editarTareaRequest = async (id, nuevosCampos) => await axios.put(`${url}/editar/${id}`, nuevosCampos);
 
-export const borrarTareaRequest = async (id) => await axios.delete("/borrar/" + id);
+export const borrarTareaRequest = async (id) => await axios.delete(`${url}/borrar/${id}`);
